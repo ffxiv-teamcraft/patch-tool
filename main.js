fa0d79c97171949234efd7bd8cdf1b0d8085caaa
Supamiu <contact@flavien-normand.fr>
2020-12-04 17:33:16 +0100

    feat: better output with added column

--- main.js
+++ main.js
@@ -25,10 +25,14 @@ const walkSync = function (dir, filelist) {
     return filelist;
 };
 
+function ignoreSheet(sheetName) {
+    return sheetName.includes('\\custom') || sheetName.includes('\\quest') || sheetName.includes('\\warp') || sheetName.includes('\\dungeon')
+}
+
 
 // TODO make this a parameter
-const previous = 'F:\\WebstormProjects\\xivapi.com\\data\\SaintCoinach.Cmd\\2020.03.27.0000.0000\\raw-exd-all';
-const update = 'C:\\Users\\Miu\\Desktop\\SaintCoinach.Cmd-master-b820-cb6a0a3\\2020.08.01.0000.0000\\raw-exd-all';
+const previous = 'F:\\WebstormProjects\\xivapi.com\\data\\ffxiv-datamining-patches\\extracts\\5.2';
+const update = 'F:\\WebstormProjects\\xivapi.com\\data\\ffxiv-datamining-patches\\extracts\\5.3';
 
 const deletedSheets = [];
 const modifiedSheets = [];
@@ -43,23 +47,64 @@ function getFirstRow(sheet) {
     return data.split(/\r?\n/)[3].split(',');
 }
 
+function getHeaders(sheet) {
+    const data = fs.readFileSync(sheet, 'utf-8');
+    return data.split(/\r?\n/)[2].split(',');
+}
+
 previousSheets.forEach(fileName => {
-    const previousHeaders = getFirstRow(fileName);
-    const sheetName = fileName.replace(previous, '');
-    const newSheet = newSheets.find(sheet => {
-        return sheet.replace(update, '') === sheetName;
-    });
-    if (newSheet === undefined) {
-        deletedSheets.push(sheetName);
-    } else {
-        const newHeaders = getFirstRow(newSheet);
-        if (newHeaders.length !== previousHeaders.length) {
-            modifiedSheets.push(sheetName);
+    if (!ignoreSheet(fileName)) {
+        const previousFirstRow = getFirstRow(fileName);
+        const sheetName = fileName.replace(previous, '');
+        const newSheet = newSheets.find(sheet => {
+            return sheet.replace(update, '') === sheetName;
+        });
+        if (newSheet === undefined) {
+            deletedSheets.push(sheetName);
+        } else {
+            const newFirstRow = getFirstRow(newSheet);
+            if (newFirstRow.length !== previousFirstRow.length) {
+                const newHeaders = getHeaders(newSheet);
+                const previousHeaders = getHeaders(fileName);
+                const entry = {
+                    name: sheetName,
+                    changes: newHeaders.reduce((changes, newHeader, index) => {
+                        if (previousHeaders[index] === newHeader) {
+                            return changes;
+                        }
+                        if (previousHeaders[index - 1] === newHeader || previousHeaders[index] === undefined) {
+                            return [
+                                ...changes,
+                                {
+                                    label: 'Inserted',
+                                    type: newHeader,
+                                    index: index
+                                }
+                            ]
+                        }
+                        if (previousHeaders[index + 1] === newHeader) {
+                            return [
+                                ...changes,
+                                {
+                                    label: 'Removed',
+                                    type: newHeader,
+                                    index: index
+                                }
+                            ]
+                        }
+                        return changes;
+                    }, [])
+                };
+                modifiedSheets.push(entry);
+            }
         }
     }
     progress.increment();
 });
 
+if (process.argv.indexOf('--quiet') > -1) {
+    return;
+}
 
 console.log('### DELETED SHEETS');
 console.log('Total : ' + deletedSheets.length);
@@ -69,17 +114,24 @@ _.uniq(deletedSheets.map(name => name.replace(/\.\w{2}\.csv/, ''))).forEach(shee
 
 console.log('### MODIFIED SHEETS');
 console.log('Total : ' + modifiedSheets.length);
-_.uniq(modifiedSheets.map(name => name.replace(/\.\w{2}\.csv/, ''))).forEach(sheet => {
-    console.log(` - ${sheet}`);
+_.uniq(modifiedSheets.map(sheet => {
+    sheet.name = sheet.name.replace(/\.\w{2}\.csv/, '');
+    return sheet;
+})).forEach(sheet => {
+    console.log(` - ${sheet.name}`);
+    sheet.changes.forEach(change => {
+        console.log(`    - ${change.label} #${change.index}: ${change.type}`);
+    })
 });
 
 console.log('### NEW SHEETS');
 const addedSheets = _.uniq(newSheets.map(name => name.replace(/\.\w{2}\.csv/, ''))).filter(sheet => {
     const newName = sheet.replace(update, '');
-    return !previousSheets.some(prev => {
+    return !ignoreSheet(newName) && !previousSheets.some(prev => {
         return prev.indexOf(newName) > -1;
     })
 });
+console.log('Total : ' + addedSheets.length);
 addedSheets.forEach(sheet => {
     console.log(` - ${sheet.replace(update, '')}`);
 });
